fc3cf867bfaa72435b27b2c8508a93fd78f3cd28
Greenreader9 <75644395+greenreader9@users.noreply.github.com>
2024-02-19 19:16:20 -0600

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -29,6 +29,20 @@ const FeatureList = [
   },
 ];
 
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <!--<div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div> COMMENT OUT SVG -->
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
 
 export default function HomepageFeatures() {
   return (
